58dda85054c0149727adb5092ed2bda4afcc8c15
Chris Hesterman <chrishesterman14@gmail.com>
2020-05-14 20:44:27 -0700

    Complete Bare Minimum Requirements

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -90,7 +90,6 @@ app.listen(port, () => console.log(`Server is running on port ${port}`));
 // The server must flatten the JSON hierarchy,
 //  mapping each item / object in the JSON to a single line of CSV report(see included sample output),
 //  where the keys of the JSON objects will be the columns of the CSV report.
-
 // You may assume the JSON data has a regular structure and hierarchy (see included sample file).
 
 // In other words,
