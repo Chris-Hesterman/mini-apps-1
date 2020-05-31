550b18d36d36d05865a36aef04bb332ddc7befb2
Chris Hesterman <chrishesterman14@gmail.com>
2020-05-31 14:17:00 -0700

    Get express up and running, html w/express static
    Still working on babel/react setup, have test get/post routes working
    in node server.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+ReactDOM.render(document.getElementById('app'), <App />);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('hey nice job');
+  res.end();
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.send('shooby doo');
+  res.end();
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('server connected at port ', PORT);
+  }
+});
