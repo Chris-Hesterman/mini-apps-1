1446ab520109e278432b23f1913cf377e6d6bf27
Chris Hesterman <chrishesterman14@gmail.com>
2020-06-07 17:31:47 -0700

    Attempt to get webpack set up.
    Not goin well yet.

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('GET req. received');
+  res.end();
+});
+
+app.post('/', (req, res) => {
+  res.send('POST req. received');
+  res.end();
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('server connected at port: ', PORT);
+});
